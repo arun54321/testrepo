aedd0b5271efb7d56fb401c39caeca12fdfe933b
arun54321 <lunar.arun@gmail.com>
2020-09-05 23:10:33 +0530

    new font

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,10 +1,11 @@
 import React from "react"
-import {Link} from "gatsby"
-import { RiHeart2Line } from "react-icons/ri";
+//import {Link} from "gatsby"
+//import { RiHeart2Line } from "react-icons/ri";
 
 const Footer = () => (
   <footer className="site-footer">
     <div className="container">
+     <p> <b>Just for fun</b></p>
     </div>
   </footer>
 )
